28778944e600e161d9a53087a0c04e930d6d54ab
1U23IT009-ANIRUDH <139449348+1U23IT009-ANIRUDH@users.noreply.github.com>
2025-02-13 09:07:08 +0000

    Done8

--- LinkedList.java
+++ LinkedList.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 public class LinkedList {
     class Node {
         int data;
         Node next;
 
-        public Node(int data) {
+public Node(int data) {
 this.data = data;
 this.next = null;
         }
@@ -19,26 +18,27 @@ this.next = null;
         if (head == null) {
             head = newNode;
             tail = newNode;
-        } else {
-           tail.next = newNode;
-            tail = newNode;
+        } 
+else 
+{
+tail.next = newNode;
+tail = newNode;
         }
     }
 
 public static void main(String[] args) {
         LinkedList list = new LinkedList();
         Scanner sc = new Scanner(System.in);
-        
-int pos=0;
-int data,data1;
+        int pos=0;
+        int data,data1;
         System.out.println("ENTER INTEGERS TO CREATE NODES (ENTER A NON-INTEGER TO EXIT):");
-
         while (sc.hasNext())
         {
+         
             if(sc.hasNextInt())
             {
-              data = sc.nextInt();
-             list.addNode(data);
+            data = sc.nextInt();
+            list.addNode(data);
             }
           else
           {
@@ -46,31 +46,37 @@ int data,data1;
             System.out.println("LINKED LIST CREATED");
             System.out.println("LINKED LIST BEFORE INSERTING");
             list.displayList();
+            //System.out.println("ENTER THE POSITON WHERE YOU WANT TO INSERT");
+            //if(sc.hasNextInt())
+            //{
+           //pos=sc.nextInt();
             System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
             if(sc.hasNextInt())
             {
             data1=sc.nextInt();
-            list.insertEnd(data1);
+            list.insertBegin(data1);
+            System.out.println("LINKED LIST AFTER INSERTING");
+            list.displayList();
             }
-            else{
+            else
+            {
               System.out.println("Enter integers only");
               System.exit(0);
             }
-            System.out.println("LINKED LIST AFTER INSERTING");
-            list.displayList();
-          }
-        }
+            }
+           }
     }
-      public void insertEnd(int data) 
-    {    
-       //TYPE YOUR CODE HERE 
-    }  
-     
+public void insertBegin(int data) {     
+    Node newNode = new Node(data);
+    newNode.next = head;
+    head = newNode;
+    System.out.println("Element Inserted");
+}
 public void displayList() {
         Node current = head;
         while (current != null) {
-System.out.print(current.data + " -> ");
-            current = current.next;
+        System.out.print(current.data + " -> ");
+        current = current.next;
         }
 System.out.println("null");
     }
